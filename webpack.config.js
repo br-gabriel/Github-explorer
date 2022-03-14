84a815f0a31cc707b73a2b530858a45a492d3abd
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-14 19:46:38 -0300

    index.html com plugin de script do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	mode: 'development',
+  	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
@@ -9,6 +11,11 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx']
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		})
+	],
 	module: {
 		rules: [
 			{
