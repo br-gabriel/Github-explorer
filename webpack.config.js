c1f379bd9bb3bb83eb0d45eee4c66ff83c998456
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-15 18:24:05 -0300

    webpackenviroment and source map added

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-	mode: 'development',
-  	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	mode: isDevelopment ? 'development' : 'production', 
+	devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+	entry: path.resolve(__dirname, 'src', 'index.jsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js'
 	},
 	resolve: {
-		extensions: ['.js', '.jsx'],
+		extensions: ['.js', '.jsx']
 	},
 	devServer: {
-		static: path.resolve(__dirname, 'public'),
+		static: path.resolve(__dirname, 'public')
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -26,6 +29,6 @@ module.exports = {
 				exclude: /node_modules/,
 				use: 'babel-loader',
 			}
-		],
+		]
 	}
 }
