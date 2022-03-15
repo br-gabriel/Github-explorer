d1d87d0a43bf661adb896e43e7c5ac7e8b19413f
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-15 20:09:31 -0300

    RepositoryItem component

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryList() {
+    return (
+        <li>
+            <strong>Unform</strong>
+            <p>Forms in React</p>
+
+            <a href="">
+                Acessar repositório
+            </a>
+        </li>
+    )
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,12 @@
+import { RepositoryItem } from './RepositoryItem'
+
 export function RepositoryList() {
     return (
         <section className="repositoryList">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem />
             </ul>
         </section>
     )
