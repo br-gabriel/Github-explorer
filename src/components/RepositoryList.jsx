89494c9c14970b5faa0e8ccd293cb39658f26b56
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-15 19:41:31 -0300

    first component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+export function RepositoryList() {
+    return (
+        <section className="repositoryList">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
