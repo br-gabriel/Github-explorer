4202b42080d8f01a1fb335716a21dcf2ff1e945d
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-16 15:43:15 -0300

    repositories scss added

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,5 @@
 import { RepositoryItem } from './RepositoryItem'
+import '../styles/repositories.scss'
 
 const repository = {
     name: 'Sem nome',
@@ -8,7 +9,7 @@ const repository = {
 
 export function RepositoryList() {
     return (
-        <section className="repositoryList">
+        <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
