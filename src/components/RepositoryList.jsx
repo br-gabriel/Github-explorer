1b0c96992dcf3791e4fde3f71a3c26a48391c110
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-03-15 21:53:43 -0300

    counter added

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,20 @@
 import { RepositoryItem } from './RepositoryItem'
 
+const repository = {
+    name: 'Sem nome',
+    description: 'Forms in React',
+    link: 'https://github.com/br-gabriel'
+}
+
 export function RepositoryList() {
     return (
         <section className="repositoryList">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem />
-                <RepositoryItem />
-                <RepositoryItem />
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
